28615f21a02131848df5cc4598fc9427fd6a3f78
Scott <scottld@stud.ntnu.no>
2025-01-09 13:45:13 +0100

    Finished the program

--- main.cpp
+++ main.cpp
@@ -2,16 +2,17 @@
 #include <vector>
 #include <thread>
 #include <cmath>
+#include <algorithm>
+#include <mutex>
 
 using namespace std;
 
-int main() {
-    vector<int> primes;
-}
-
 class FindPrimes {
 public:
-    bool isPrime(int num) {
+    static vector<int> primes;
+    static mutex primeMutex;
+
+    static bool isPrime(int num) {
         if (num == 2) return true;
         if (num % 2 == 0) return false;
         if (num == 1) return false;
@@ -21,7 +22,53 @@ public:
         return true;
     }
 
-    void findPrimesInRangeWithThreads(int start, int end, int numThreads) {
+    static vector<int> getPrimesInRangeWithThreads(int start, int end, int numThreads) {
+        vector<thread> threads;
+        int delta = (end - start) / (numThreads);
 
+        int threadNum = 0;
+        for (int i = start; i <= end; i += delta) {
+            threadNum++;
+            int currentEnd = i+delta-1;
+            if (threadNum == numThreads) {
+                currentEnd = end;
+                threads.emplace_back(findPrimeInRangeAndAppendToPrimeVector, i, currentEnd);
+                break;
+            }
+            threads.emplace_back(findPrimeInRangeAndAppendToPrimeVector, i, currentEnd);
+        }
+
+        for(thread &thread : threads) {
+            thread.join();
+        }
+
+        sortPrimes();
+
+        return primes;
+    }
+
+    static void findPrimeInRangeAndAppendToPrimeVector(int start, int end) {
+        for (int i = start; i <= end; i ++) {
+            if (isPrime(i)) {
+                lock_guard<mutex> lock(primeMutex);
+                primes.push_back(i);
+            }
+        }
+    }
+
+    static void sortPrimes() {
+        sort(primes.begin(), primes.end());
     }
 };
+
+vector<int> FindPrimes::primes;
+mutex FindPrimes::primeMutex;
+
+int main() {
+    vector<int> primes;
+    primes = FindPrimes::getPrimesInRangeWithThreads(1, 100, 6);
+
+    for (int num : primes) {
+        cout << num << endl;
+    }
+}
