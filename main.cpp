0bad2833776bd81786bab5520cef19946167c18f
Scott <scottld@stud.ntnu.no>
2025-01-09 12:44:15 +0100

    Added prime check alg to code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <cmath>
 
 using namespace std;
 
@@ -8,5 +10,18 @@ int main() {
 }
 
 class FindPrimes {
+public:
+    bool isPrime(int num) {
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        if (num == 1) return false;
+        for (int i = 3; i < sqrt(num); i+=2) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
 
+    void findPrimesInRangeWithThreads(int start, int end, int numThreads) {
+
+    }
 };
